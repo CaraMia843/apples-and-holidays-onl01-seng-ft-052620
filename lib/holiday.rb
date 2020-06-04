8647dd51e1037ed8c93273bb69c3a1d05cbaf8dc
Cara Zellner <carajeanine@gmail.com>
2020-06-04 19:16:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |holiday_event, supply|
+    put " #{holiday_event.to_s.split("_").each{|element| "
 
 end
 
